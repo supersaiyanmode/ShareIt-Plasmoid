844084b7c909f65b7667da4bb4b4845466a8cda5
thrustmaster <supersaiyanmode.rox@gmail.com>
2012-06-05 22:34:35 +0530

    Check if the current post is a code, if so render HTML and store to database

--- Server/paste.py
+++ Server/paste.py
@@ -5,8 +5,12 @@ from uuid import uuid4
 import ShortLink
 import mimetypes
 import Paste
+import os
+from google.appengine.ext.webapp.template import render
+
 
 APP_KEY = "OWI0NDdhMDktMzJmNC00NGIyLTlmOTMtODA1NGNmNjA5M2QyZDAzZjMyZmMtZGYzMi00NTdjLWI0"
+
 class Paster(webapp.RequestHandler):
     def post(self):
         key = self.request.get("key")
@@ -23,13 +27,22 @@ class Paster(webapp.RequestHandler):
         mime = self.request.get("mime") or "application/octet-stream"
         
         keyName = str(uuid4())
-        Paste.Paste(title=title, content=str(content),key_name=keyName, mime=mime).put()
+        
+        #check if paste is a code
+        if self.request.get("type") == "code":
+            data = {"code":content}
+            path = os.path.join(os.path.dirname(__file__), 'html/pasteShow.html')
+            content = render(path, {'DATA':data})
+            mime = "text/html"
+        
+        Paste.Paste(title=title, content=db.Blob(str(content)),key_name=keyName, mime=mime).put()
         
         url = self.request.url[:self.request.url.index("/paste")] + "/" + keyName + "/paste"
         
         #Add a possible extension to the link
         ext = mimetypes.guess_extension(mime)
-        if ext: url += ext
+        if ext:
+            url += ext
         
         self.response.out.write(json.dumps({"result":"success", "url":ShortLink.getShortUrl(url)}))
         self.response.out.write("\r\n")
